1bf2f749ae4ad7bd14c64eb494d906f79df6f549
Matheus Paz <matheusreispaz@gmail.com>
2021-12-20 09:13:03 -0300

    Modified editor and prettier, created Display component and adjusted app

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   bracketSpacing: true,
-  jsxBracketSameLine: true,
+  bracketSameLine: true,
   singleQuote: true,
   trailingComma: 'all',
   arrowParens: 'avoid',
+  semi: true
 };
